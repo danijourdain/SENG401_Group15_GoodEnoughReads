0c3f784377097dfb512eb53e49f28db40c131b0d
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2023-02-28 11:01:08 -0700

    statistics and database

---
+++ GoodEnoughReads/Statistics/DatabaseConnect.py
@@ -0,0 +1,43 @@
+import mysql.connector 
+
+class DatabaseConnect:
+
+    def __init__(self):
+        # p = input("Enter Database Password: ")
+        # u = input("Enter Database Username: ")
+
+        try:
+            self.connection = mysql.connector.connect(user ='CPSC', password = '471', host='127.0.0.1', database='471')
+
+        except mysql.connector.Error as error:
+            print("Failed to create table in MySQL: {}".format(error))
+
+    def getTable(self, s, table):
+        mycursor = self.connection.cursor()
+
+        mycursor.execute(f"SELECT {s} FROM {table}")
+
+        myresult = mycursor.fetchall()
+        return myresult
+
+    def performQuery(self, query):
+        mycursor = self.connection.cursor()
+
+        mycursor.execute(query)
+
+        myresult = mycursor.fetchall()
+        return myresult
+
+    def insert(self, query):
+        mycursor = self.connection.cursor()
+
+        mycursor.execute(query)
+        self.connection.commit()
+
+    def close(self):
+        if self.connection.is_connected():
+            self.connection.close()
+
+if __name__ == "__main__":
+    database = DatabaseConnect()
+    database.close()
