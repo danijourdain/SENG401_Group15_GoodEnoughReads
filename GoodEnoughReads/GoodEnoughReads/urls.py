08b59546834e44e09ab2ad1a4b55798a2bb161c6
danijourdain <danijourdain@icloud.com>
2023-03-08 09:57:00 -0700

    Worked on Login/Create User
    they work and add users to auth_user table, but do not add to User table. Login doesn't get username correctly

--- GoodEnoughReads/GoodEnoughReads/urls.py
+++ GoodEnoughReads/GoodEnoughReads/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('', include('gersiteapp.urls')),
     path('', include('search.urls')),
     path('', include('Collections.urls')),
--- GoodEnoughReads/ManageAccount/viewsManageAccount.py
+++ GoodEnoughReads/ManageAccount/viewsManageAccount.py
@@ -1,14 +1,51 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
 def logout_view(request):
     return redirect('login')
 
-def login(request):
-    return render(request, 'ManageAccount/login.html')
+# def login(request):
+#     return render(request, 'ManageAccount/registration/login.html')
 
 def signup(request):
-    return render(request, 'ManageAccount/signup.html')
+    if request.method == 'POST':
+        email = request.POST.get('email', '')
+        fname = request.POST.get('fname', '')
+        lname = request.POST.get('lname', '')
+        uname = request.POST.get('username', '')
+        pw1 = request.POST.get('password', '')
+        pw2 = request.POST.get('password2', '')
+        user = authenticate(request, email=email, password=pw1)
+
+        if user is None and pw1 == pw2:
+            user = User.objects.create_user(username=uname, password=pw1, email=email)
+            user.first_name = fname
+            user.last_name = lname
+            user.save()
+            return redirect('/welcome/')
+        else:
+            error_message = "Invalid login credentials"
+            return render(request, 'ManageAccount/registration/signup.html', {'error_message': error_message})
+    else:
+        return render(request, 'ManageAccount/signup.html')
 
 def reset_password(request):
     return render(request, 'ManageAccount/reset_password.html')
+
+def login(request):
+    if request.method == 'POST':
+        print("HERE!!!")
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(request, username=username, password=password)
+        print(user.get_username)
+        if user is not None:
+            context = {'username': username}
+            return redirect('/welcome/', context)
+        else:
+            error_message = "Invalid login credentials"
+            return render(request, 'ManageAccount/registration/login.html', {'error_message': error_message})
+    else:
+        return render(request, 'ManageAccount/registration/login.html')
+    
