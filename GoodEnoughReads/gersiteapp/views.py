65826a83cb30be5360b42312df6396a8704c36fe
Orionahn <101444254+Orionahn@users.noreply.github.com>
2023-03-18 20:44:31 -0600

    Making homepage a little fancy
    There's an issue with the username not being carried through. Need session variable to change render to redirect in viewsManageAccount for username if we want it to be printed when you log in.

--- GoodEnoughReads/gersiteapp/views.py
+++ GoodEnoughReads/gersiteapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+import requests
 from . import LoginModel
 
 def welcome(request):
-    return render(request, 'gersiteapp/welcome.html')
+    # Make API call to Open Library to retrieve books by genre
+    genre = 'fiction'
+    url = f'https://openlibrary.org/subjects/{genre}.json'
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract information about books and their covers
+    books = []
+    for work in data['works']:
+        book = {}
+        book['title'] = work['title']
+        book['author'] = work['authors'][0]['name']
+        book['cover'] = f'https://covers.openlibrary.org/b/id/{work["cover_id"]}-M.jpg'
+        books.append(book)
+
+    return render(request, 'gersiteapp/welcome.html', {'books': books})
 
 def shelf(request):
     return render(request, 'gersiteapp/shelf.html')
