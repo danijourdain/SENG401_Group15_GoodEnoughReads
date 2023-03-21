b983e783e200cd6d3e68219f2deeed5b88f2ba7a
Maitry Rohit <101202431+MaitryRohit@users.noreply.github.com>
2023-03-21 13:32:27 -0600

    Add to user collection query needs fixing

--- GoodEnoughReads/Statistics/viewsAwards.py
+++ GoodEnoughReads/Statistics/viewsAwards.py
@@ -14,6 +14,7 @@ def awards(request):
     Image = "gersiteapp/static/gersiteapp/img/Awards/"
 
     if UserXP > ReqXP:
+        # pass
         Userlevel = awd.updateUserLevel()
 
     if(Userlevel < 6):
