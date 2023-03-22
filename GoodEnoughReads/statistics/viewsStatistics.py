08379fc10925a9ce3c2a0930fefe67de8ce0afb1
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2023-03-22 13:43:29 -0600

    fixes

--- GoodEnoughReads/statistics/viewsStatistics.py
+++ GoodEnoughReads/statistics/viewsStatistics.py
@@ -6,7 +6,12 @@ def statistics(request):
     # hard coded email (for now)
     email = request.session['email']
     stats = StatisticsModel.StatisticsModel(email)
+    
     pages_this_week = stats.NumPagesThisWeek()
+    if pages_this_week == 0:
+        Opening_message = "You got this! Try reading more next week"
+    else:
+        Opening_message = "Congratulations"
     
     books_this_month, books_per_month = stats.CalculateBooksPerMonth()
     stats.CreateBarGraph(list(books_per_month.keys()), list(books_per_month.values()), "Months", "Number of Books Read", "Number of Books you Read per Month this Year")
@@ -22,7 +27,13 @@ def statistics(request):
 
     books_reread_the_most = stats.CalcBookReread()
 
+    if len(books_reread_the_most) > 0:
+        books_reread_message = "You reread <Strong>" + books_reread_the_most + "</Strong> the most! You must really love it."
+    else:
+        books_reread_message = "You haven't reread any books."
+
     return render(request, 'Statistics/statistics.html', {'pages_this_week': pages_this_week, 
                                                           'books_this_month': books_this_month,
                                                           'books_this_year': books_this_year,
-                                                          'books_reread': books_reread_the_most})
+                                                          'books_reread': books_reread_message,
+                                                          'Opening_Message' : Opening_message})
