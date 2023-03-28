1b7ea9aa59b1cf6e67209197a7debfd090545ca9
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2023-03-28 15:18:17 -0600

    Added comments to awards

--- GoodEnoughReads/Statistics/Awards.py
+++ GoodEnoughReads/Statistics/Awards.py
@@ -1,10 +1,5 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from matplotlib.font_manager import FontProperties
-# from matplotlib.ticker import MaxNLocator
 from django.db import models
 from django.db import connection, transaction
-# import datetime
 
 
 class Awards:
@@ -12,30 +7,29 @@ class Awards:
         self.email = email
         self.cursor = connection.cursor()
 
-    # def getImage(self, level):
-    #     self.cursor.execute("SELECT Image FROM Awards WHERE Level = ?;", level)
-    #     return self.cursor.fetchall()[0]
-    
     def getReqXP(self, level):
+        # get XP to reach level
         level = str(level)
         self.cursor.execute("SELECT Required_XP FROM Awards WHERE Level = %s;", [level])
         return self.cursor.fetchall()[0][0]
     
     def getUserXP(self):
+        # get XP user currently has
         self.cursor.execute("SELECT XP FROM User WHERE Email = %s;", [self.email])
         return self.cursor.fetchall()[0][0]
 
     def getUserLevel(self):
+        # get current user level
         self.cursor.execute("SELECT AwardProfile FROM User WHERE Email = %s;", [self.email])
         self.UserLevel = self.cursor.fetchall()[0][0]
         return self.UserLevel
     
     def updateUserLevel(self):
+        # update user level with 1 + self.userlevel
         self.cursor.execute("UPDATE User SET AwardProfile = %s WHERE Email = %s;", [self.UserLevel + 1, self.email])
-        # self.cursor.execute("SELECT AwardProfile FROM User WHERE Email = %s;", [self.email])
-        # return self.cursor.fetchall()[0][0]
         
     def updateUserXP(self, xp):
+        # update user XP to xp
         self.cursor.execute("UPDATE User SET XP = %s WHERE Email = %s;", [xp, self.email])
 
 
